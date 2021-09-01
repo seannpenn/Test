4711abf785a1df9218549813e6fa61a6c88176d0
Sean Pinote <sean.pinote@usjr.edu.ph>
2021-09-01 15:54:44 +0800

    alert

--- behavior.js
+++ behavior.js
@@ -1,3 +1,17 @@
-function alertMe(a) {
-    alert(a);
+function validate() {
+    var text = document.getElementById("text").value;
+    var password = document.getElementById("password").value;
+
+    if (text == "admin" && password == "pass") {
+
+        alert("Welcome Sean Milbert!");
+        return false;
+    } else {
+        alert("Enter your right credentials!!");
+    }
+
+
+
+
+
 }
